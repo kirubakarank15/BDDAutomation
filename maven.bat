6e6419892ea2437fc7522c48ee1e7e94ce0a0d61
Raja Praveen Pydimarri <Pydimarri_Raja_Praveen_D@cat.com>
2017-09-01 13:20:52 +0000

    Updated maven.bat

--- maven.bat
+++ maven.bat
@@ -1,4 +1,4 @@
 @Echo off
 Echo Current dir: "%CD%"
 
-call C:\java\maven\apache-maven-3.2.2\bin\mvn.bat install:install-file -Dfile=src/main/lib/PwdManager.jar -DgroupId=PwdManager  -DpomFile=src/main/lib/PwdManager-1.0.pom -DartifactId=PwdManager -Dversion=1.0 -Dpackaging=jar
+call C:\java\maven\apache-maven-3.2.2\bin\mvn.bat install:install-file -Dfile=src/PwdManager.jar -DgroupId=PwdManager  -DpomFile=src/PwdManager-1.0.pom -DartifactId=PwdManager -Dversion=1.0 -Dpackaging=jar
