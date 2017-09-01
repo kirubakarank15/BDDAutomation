1d1fd9c52445c0d6a167f701c5dc06a9dfd75c2e
Raja Praveen Pydimarri <Pydimarri_Raja_Praveen_D@cat.com>
2017-09-01 13:11:35 +0000

    Added file maven.bat

---
+++ maven.bat
@@ -0,0 +1,4 @@
+@Echo off
+Echo Current dir: "%CD%"
+
+call C:\java\maven\apache-maven-3.2.2\bin\mvn.bat install:install-file -Dfile=DealerServiceCommon/src/main/lib/PwdManager.jar -DgroupId=PwdManager  -DpomFile=DealerServiceCommon/src/main/lib/PwdManager-1.0.pom -DartifactId=PwdManager -Dversion=1.0 -Dpackaging=jar
