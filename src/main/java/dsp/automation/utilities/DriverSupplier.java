f7c415ede49d176d6e43b0a58fc06a890aafd25b
Kirubakaran Krishnan <Krishnan_Kirubakaran@cat.com>
2017-09-04 13:09:24 +0000

    Updated DriverSupplier.java

--- src/main/java/dsp/automation/utilities/DriverSupplier.java
+++ src/main/java/dsp/automation/utilities/DriverSupplier.java
@@ -21,7 +21,7 @@ public class DriverSupplier {
         options.addArguments("--ignore-certificate-errors");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("disable-infobars"); 
-              options.setBinary("Resources\\chromedriver.exe");
+             //options.setBinary("Resources\\chromedriver.exe");
         //options.addArguments("--incognito");
        // options.addArguments("--disable-extensions");
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
