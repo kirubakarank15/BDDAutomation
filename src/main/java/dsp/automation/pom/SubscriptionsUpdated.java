a723d69992dbec6ef2fa663af5cfff78e2b139a3
AP\krishk10 <krishnan_kirubakaran@cat.com>
2017-10-06 20:02:06 +0530

    # DSP_Automation7

--- src/main/java/dsp/automation/pom/SubscriptionsUpdated.java
+++ src/main/java/dsp/automation/pom/SubscriptionsUpdated.java
@@ -3,6 +3,8 @@
  */
 package dsp.automation.pom;
 
+import static org.testng.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
@@ -16,6 +18,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 
 import dsp.automation.utilities.CustomisedException;
+import dsp.automation.utilities.FileHandling;
 import dsp.automation.utilities.TestFunctionsFactory;
 
 /**
@@ -32,7 +35,9 @@ public class SubscriptionsUpdated {
 	@FindBy(xpath = "//div//span[contains(text(),'/N')]//following-sibling::span")
 	private WebElement lblEquipmentSerialNumber;
 
-	@FindBy(xpath = "//button[contains(text(),'Save / Sync')]")
+	@FindBy(xpath = "//button[contains(text(),'
+						Submit
+					')]")
 	private WebElement btnSaveSync;
 	@FindBy(xpath = "//div[contains(@class,'services')]//span[contains(text(),'Services:')]//parent::div//select")
 
@@ -98,7 +103,7 @@ public class SubscriptionsUpdated {
 	 * 
 	 * @throws CustomisedException
 	 */
-	public void servicesdrpdownsynccheck(String data) throws CustomisedException {
+	public void servicesDrpDownSyncCheck(String data) throws CustomisedException {
 
 		fieldValue = "Application Services DropDown";
 		List<String> defaultValues = new ArrayList<>(
@@ -130,7 +135,7 @@ public class SubscriptionsUpdated {
 	 * 
 	 * @throws CustomisedException
 	 */
-	public void customerAndBaseSubscriptiondrpdownsynccheck(String data) throws CustomisedException {
+	public void customerAndBaseSubscriptionDrpDownSyncCheck(String data) throws CustomisedException {
 
 		fieldValue = "Customer Subscription DropDown";
 		try {
@@ -160,7 +165,7 @@ public class SubscriptionsUpdated {
 	 * 
 	 * @throws CustomisedException
 	 */
-	public void dealerdrpdownsynccheck(String data) throws CustomisedException {
+	public void dealerDrpDownSyncCheck(String data) throws CustomisedException {
 
 		fieldValue = "Customer Subscription DropDown";
 		try {
@@ -190,7 +195,7 @@ public class SubscriptionsUpdated {
 	 * 
 	 * @throws CustomisedException
 	 */
-	public void catdrpdownsynccheck(String data) throws CustomisedException {
+	public void catDrpDownSyncCheck(String data) throws CustomisedException {
 
 		fieldValue = "Customer Subscription DropDown";
 		try {
@@ -213,8 +218,52 @@ public class SubscriptionsUpdated {
 	}
 
 	/*
-	 * krishk10
-	 * This method is for setting subscriptions
+	 * krishk10 
+	 * Method to check the applicale add-Ons are displayed or not
+	 * @param data
+	 * 
+	 * @throws CustomisedException
+	 */
+
+	public void addOnValuesCheck(String data) throws CustomisedException {
+
+		fieldValue = "Customer Subscription DropDown";
+		try {
+			String[] inputData = data.split(",");
+			List<WebElement> chkBoxOptions = chkBoxAdditonalServices;
+			int i = 0;
+			for (WebElement chkBox : chkBoxOptions) {
+				i++;
+
+				LOGGER.info("checking the AddOn Value :" + inputData);
+				for (String inputAddOn : inputData) {
+					if (chkBox.getText().trim().equalsIgnoreCase(inputAddOn.trim())) {
+
+					} else if (chkBoxOptions.size() == i) {
+						throw new CustomisedException("The following add-On is not available for this Serial No :",
+								inputAddOn);
+					}
+
+					break;
+				}
+			}
+
+		} catch (Exception e) {
+			if (!CustomisedException.getFieldValue().equals(null)) {
+				throw new CustomisedException(
+						"cat Subscription DropDown is facing problem with" + CustomisedException.getFieldValue(),
+						CustomisedException.getErrorMessage());
+			} else {
+				throw new CustomisedException(fieldValue, e.getMessage().toString());
+			}
+
+		}
+
+	}
+
+	/*
+	 * krishk10 This method is for setting subscriptions
+	 * 
 	 * @param testData
 	 * 
 	 * @throws CustomisedException
@@ -286,4 +335,278 @@ public class SubscriptionsUpdated {
 		}
 
 	}
+
+	/*
+	 * krishk10 This method is for verifying the already set subscriptions
+	 * 
+	 * @param testData
+	 * 
+	 * @throws CustomisedException
+	 */
+	public void verifySubscriptionValues(String drpDwnName, String testData) throws CustomisedException {
+
+		fieldValue = drpDwnName;
+		try {
+
+			switch (drpDwnName.toLowerCase().trim().replaceAll(" ", "")) {
+			// SubscripitionScreens
+			case "s/n":
+			case "serialno:":
+
+				TestFunctionsFactory.webWait(30, TestFunctionsFactory.driver
+						.findElement(By.xpath("//div[contains(@title,'S/N " + testData + "')]")));
+				break;
+			case "services":
+			case "services:":
+				TestFunctionsFactory.compareSelectedValue(drpDwnServices, testData);
+				break;
+			case "customer":
+			case "customer:":
+				TestFunctionsFactory.compareSelectedValue(drpDwnCustomer, testData);
+				break;
+			case "cat":
+			case "cat:":
+				TestFunctionsFactory.compareSelectedValue(drpDwnCat, testData);
+				break;
+			case "dealer:":
+			case "dealer":
+				TestFunctionsFactory.compareSelectedValue(drpDwnDealer, testData);
+				break;
+			case "additionalservices":
+			case "additionalservices:":
+
+				List<WebElement> chkBoxOptions = chkBoxAdditonalServices;
+				int i = 0;
+				for (WebElement chkBox : chkBoxOptions) {
+					i++;
+					String[] addOnServices = testData.split(",");
+					LOGGER.info("checking the AddOn Value :" + addOnServices);
+					for (String inputAddOn : addOnServices) {
+						if (chkBox.getText().trim().equalsIgnoreCase(inputAddOn.trim())) {
+
+							TestFunctionsFactory.verifyElementdisplayed(TestFunctionsFactory.driver
+									.findElement(By.xpath("//li[" + i + "]//input[contains(@class,'ng-not-empty')]")));
+
+						}
+
+						break;
+					}
+				}
+			}
+		} catch (Exception e) {
+			CustomisedException.setErrorMessage(e.getMessage().toString());
+			if (!CustomisedException.getFieldValue().equals(null)) {
+				throw new CustomisedException(
+						"Subscription page is facing problem with " + CustomisedException.getFieldValue(),
+						CustomisedException.getErrorMessage());
+			} else {
+
+				throw new CustomisedException(fieldValue, e.getMessage().toString());
+
+			}
+
+		}
+
+	}
+
+	/*
+	 * krishk10 This method is for handling billing pop up
+	 * 
+	 * @param testData
+	 * 
+	 * @throws CustomisedException
+	 */
+	public void selectBillingDetails(String drpDwnName, String testData) throws CustomisedException {
+
+		fieldValue = drpDwnName;
+		try {
+
+			switch (drpDwnName.toLowerCase().trim().replaceAll(" ", "")) {
+			case "accountnameordescription:":
+			case "accountnameordescription":
+				TestFunctionsFactory.selectFromDropDown(drpDownAccountDesc, testData);
+				break;
+			case "accountnumber:":
+			case "accountnumber":
+				TestFunctionsFactory.selectFromDropDown(drpDownAccountNum, testData);
+				break;
+			case "accountcontact:":
+			case "accountcontact":
+				TestFunctionsFactory.selectFromDropDown(drpDownAccountContact, testData);
+				break;
+			}
+
+		} catch (Exception e) {
+			CustomisedException.setErrorMessage(e.getMessage().toString());
+			if (!CustomisedException.getFieldValue().equals(null)) {
+				throw new CustomisedException(
+						"Billing Pop Up is facing problem with " + CustomisedException.getFieldValue(),
+						CustomisedException.getErrorMessage());
+			} else {
+
+				throw new CustomisedException(fieldValue, e.getMessage().toString());
+
+			}
+
+		}
+
+	}
+
+	/*
+	 * krishk10 This method is for handling cancel Reason pop up
+	 * 
+	 * @param testData
+	 * 
+	 * @throws CustomisedException
+	 */
+	public void selectCancelReason(String drpDwnName, String testData) throws CustomisedException {
+
+		fieldValue = drpDwnName;
+		try {
+
+			switch (drpDwnName.toLowerCase().trim().replaceAll(" ", "")) {
+			case "reason:":
+			case "reason":
+			case "cancellationreason":
+			case "cancelreason":
+				TestFunctionsFactory.selectFromDropDown(drpDownCancelPopUp, testData);
+				break;
+			}
+
+		} catch (Exception e) {
+			CustomisedException.setErrorMessage(e.getMessage().toString());
+			if (!CustomisedException.getFieldValue().equals(null)) {
+				throw new CustomisedException(
+						"Cancel Reason Pop Up is facing problem with " + CustomisedException.getFieldValue(),
+						CustomisedException.getErrorMessage());
+			} else {
+
+				throw new CustomisedException(fieldValue, e.getMessage().toString());
+
+			}
+
+		}
+
+	}
+
+	/*
+	 * krishk10 This methods helps for doing different button clicks in
+	 * subscriptions page.
+	 * 
+	 * @param action
+	 * 
+	 * @throws CustomisedException
+	 * 
+	 * @throws Exception
+	 */
+	public void actionsSubscripition(String action) throws CustomisedException, Exception {
+
+		fieldValue = action;
+		try {
+
+			switch (action.toUpperCase()) {
+			case "SUBMIT":
+			case "REVIEW":
+			case "SAVE/SYNC":
+				TestFunctionsFactory.javaScriptClick(btnSaveSync);
+				TestFunctionsFactory.waitForPageLoaded();
+				break;
+			case "<<Back":
+			case "BACK":
+				TestFunctionsFactory.javaScriptClick(linkback);
+				TestFunctionsFactory.waitForPageLoaded();
+				break;
+			case "OK":
+				TestFunctionsFactory.webWait(10, btnOK);
+				TestFunctionsFactory.javaScriptClick(btnOK);
+				TestFunctionsFactory.waitForPageLoaded();
+				break;
+
+			case "CANCEL":
+				TestFunctionsFactory.javaScriptClick(btnCancelPopUp);
+				TestFunctionsFactory.waitForPageLoaded();
+				break;
+			case "CONFIRM":
+				TestFunctionsFactory.javaScriptClick(btnConfirmBillingPopUp);
+				TestFunctionsFactory.waitForPageLoaded();
+				break;
+			default:
+				throw new CustomisedException(fieldValue,
+						"The Given Action is not a valid one for this page, please correct it");
+
+			}
+
+		} catch (Exception e) {
+			if (!CustomisedException.getFieldValue().equals(null)) {
+				throw new CustomisedException(
+						"Clciking on " + action + " button is facing problem :" + CustomisedException.getFieldValue(),
+						CustomisedException.getErrorMessage());
+			} else {
+				throw new CustomisedException(fieldValue, e.getMessage().toString());
+			}
+
+		}
+
+	}
+
+	/*
+	 * krishk10
+	 * 
+	 * @throws CustomisedException
+	 * 
+	 * @throws Exception
+	 */
+	public void VerifySelectCustomerWarningPopUp() throws CustomisedException, Exception {
+
+		fieldValue = "Select Customer Error PopUp";
+		try {
+			TestFunctionsFactory.verifyElementdisplayed(titleSelectCustErrPopUp);
+			assertEquals(messageSelectCustErrPopUp.getText().trim().toLowerCase(),
+					(FileHandling.getProperty("SUBSCRIPTION_NO_CUSTOMER_WARN")).trim().toLowerCase());
+
+		} catch (Exception e) {
+			if (!CustomisedException.getFieldValue().equals(null)) {
+				throw new CustomisedException(
+						"Subscriptions page is facing problem with" + CustomisedException.getFieldValue(),
+						CustomisedException.getErrorMessage());
+			} else {
+				throw new CustomisedException(fieldValue, e.getMessage().toString());
+			}
+
+		}
+
+	}
+
+	/*
+	 * krishk10
+	 * 
+	 * @throws CustomisedException
+	 * 
+	 * @throws Exception
+	 */
+	public void VerifyUnSavedSubscriptionWarningPopUp() throws CustomisedException, Exception {
+
+		fieldValue = "Unsaved Subscription warning PopUp";
+		try {
+			System.out.println(messageUnsavedWarning.getText());
+
+			/*
+			 * assertEquals(messageSelectCustErrPopUp.getText().trim().
+			 * toLowerCase(),
+			 * (FileHandling.getProperty("SUBSCRIPTION_NO_CUSTOMER_WARN")).trim(
+			 * ).toLowerCase());
+			 */
+		} catch (Exception e) {
+			if (!CustomisedException.getFieldValue().equals(null)) {
+				throw new CustomisedException(
+						"Subscriptions page is facing problem with" + CustomisedException.getFieldValue(),
+						CustomisedException.getErrorMessage());
+			} else {
+				throw new CustomisedException(fieldValue, e.getMessage().toString());
+			}
+
+		}
+
+	}
+
 }
---
+++ src/test/java/stepDefenition/SubscripitionsNew.java
@@ -0,0 +1,209 @@
+/**
+ * 
+ */
+package stepDefenition;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import dsp.automation.pom.Subscriptions;
+import dsp.automation.pom.SubscriptionsUpdated;
+import dsp.automation.utilities.CustomisedException;
+import dsp.automation.utilities.TestFunctionsFactory;
+
+/**
+ * @author Kirubakaran.K(Krishk10) DSP-ISSS Cucumber Automation
+ * 
+ */
+public class SubscripitionsNew {
+
+	@When("^verify the services drop down is displayed with applicable \"([^\"]*)\" values for the device type \"([^\"]*)\"$")
+	public void verifyServices(String arg1, String arg2) {
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
+		try {
+			SubscriptionsUpdated obj = PageFactory.initElements(TestFunctionsFactory.driver,
+					SubscriptionsUpdated.class);
+			obj.servicesDrpDownSyncCheck(arg1);
+		} catch (Exception e) {
+
+			if (CustomisedException.getErrorMessage() != null) {
+				TestFunctionsFactory.takeSnapShot("ServicesDropDown");
+				Assert.fail(CustomisedException.getFieldValue() + " :" + CustomisedException.getErrorMessage());
+			} else {
+				e.printStackTrace();
+				TestFunctionsFactory.takeSnapShot("ServicesDropDown");
+				Assert.fail(e.getMessage().toString());
+			}
+		}
+	}
+
+	@Then("^select the services as \"([^\"]*)\" from subscriptions page$")
+	public void selectServices(String data) throws Throwable {
+
+		try {
+
+			SubscriptionsUpdated obj = PageFactory.initElements(TestFunctionsFactory.driver,
+					SubscriptionsUpdated.class);
+			obj.enterSubscriptionValues("services", data);
+
+		}
+
+		catch (Exception e) {
+
+			if (CustomisedException.getErrorMessage() != null) {
+				TestFunctionsFactory.takeSnapShot("Subscriptions-Services DropDown");
+				Assert.fail(CustomisedException.getFieldValue() + " :" + CustomisedException.getErrorMessage());
+			} else {
+
+				e.printStackTrace();
+				TestFunctionsFactory.takeSnapShot("Subscriptions-Services DropDown");
+				Assert.fail(e.getMessage().toString());
+			}
+		}
+
+	}
+
+	@When("^verify whether the subscription dropdown values \"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\"  are populated according to the selected service$")
+	public void verifySubscriptionDrpDwn(String customer, String dealer, String cat) {
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
+		try {
+			SubscriptionsUpdated obj = PageFactory.initElements(TestFunctionsFactory.driver,
+					SubscriptionsUpdated.class);
+			if (customer != null) {
+				obj.customerAndBaseSubscriptionDrpDownSyncCheck(customer);
+			}
+
+			if (dealer != null) {
+				obj.dealerDrpDownSyncCheck(dealer);
+			}
+			if (cat != null) {
+				obj.catDrpDownSyncCheck(cat);
+			}
+
+		} catch (Exception e) {
+
+			if (CustomisedException.getErrorMessage() != null) {
+				TestFunctionsFactory.takeSnapShot("Subscriptions DropDown");
+				Assert.fail(CustomisedException.getFieldValue() + " :" + CustomisedException.getErrorMessage());
+			} else {
+				e.printStackTrace();
+				TestFunctionsFactory.takeSnapShot("Subscriptions DropDown");
+				Assert.fail(e.getMessage().toString());
+			}
+		}
+	}
+
+	@Then("^select the subscriptions as \"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\"$")
+	public void selectSubscriptions(String customer, String dealer, String cat) throws Throwable {
+
+		try {
+
+			SubscriptionsUpdated obj = PageFactory.initElements(TestFunctionsFactory.driver,
+					SubscriptionsUpdated.class);
+			if (customer != null) {
+				obj.enterSubscriptionValues("customer", customer);
+			}
+			if (dealer != null) {
+				obj.enterSubscriptionValues("dealer", dealer);
+			}
+
+			if (cat != null) {
+				obj.enterSubscriptionValues("cat", cat);
+			}
+
+		}
+
+		catch (Exception e) {
+
+			if (CustomisedException.getErrorMessage() != null) {
+				TestFunctionsFactory.takeSnapShot("Subscriptions-Services DropDown");
+				Assert.fail(CustomisedException.getFieldValue() + " :" + CustomisedException.getErrorMessage());
+			} else {
+
+				e.printStackTrace();
+				TestFunctionsFactory.takeSnapShot("Subscriptions-Services DropDown");
+				Assert.fail(e.getMessage().toString());
+			}
+		}
+
+	}
+
+	@When("^verify whether the Add-On services are displayed as \"([^\"]*)\"$")
+	public void verifyAddOnServices(String addOnServices) {
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
+		try {
+			SubscriptionsUpdated obj = PageFactory.initElements(TestFunctionsFactory.driver,
+					SubscriptionsUpdated.class);
+			obj.addOnValuesCheck(addOnServices);
+		} catch (Exception e) {
+
+			if (CustomisedException.getErrorMessage() != null) {
+				TestFunctionsFactory.takeSnapShot("Add-Onservices");
+				Assert.fail(CustomisedException.getFieldValue() + " :" + CustomisedException.getErrorMessage());
+			} else {
+				e.printStackTrace();
+				TestFunctionsFactory.takeSnapShot("Add-Onservices");
+				Assert.fail(e.getMessage().toString());
+			}
+		}
+	}
+
+	@When("^Set Subscription details as \"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\"$")
+	public void set_Subscription_details_as(String arg1, String arg2, String arg3, String arg4, String arg5)
+			throws Throwable {
+
+		if (arg1 != null) {
+			System.out.println(arg1);
+		}
+		if (arg2 != null) {
+			System.out.println(arg2);
+		}
+		if (arg3 != null) {
+			System.out.println(arg3);
+		}
+		if (arg4 != null) {
+			System.out.println(arg4);
+		}
+		if (arg5 != null) {
+			System.out.println(arg5);
+		}
+
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
+	}
+
+	@When("^submit subscription details$")
+	public void submit_subscription_details() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
+	}
+
+	@Then("^Verify Subscription details are displayed with \"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\" in My Worklist page$")
+	public void verify_Subscription_details_should_be_displayed_with_in_My_Worklist_page(String arg1, String arg2,
+			String arg3, String arg4, String arg5, String arg6) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		// System.out.println(arg1 + " " + arg2 + " " + arg3 + " " + arg4 + " "
+		// + arg5 + " " + arg6);
+		// throw new PendingException();
+	}
+
+	@When("^cancel subscription by selecting \"([^\"]*)\"$")
+	public void cancel_subscription_by_selecting(String arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
+	}
+
+	@When("^select account name \"([^\"]*)\",\"([^\"]*)\",\"([^\"]*)\" from billing Pop Up$")
+	public void select_account_name_from_billing_Pop_Up(String arg1, String arg2, String arg3) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		// System.out.println(arg1 + " " + arg2 + " " + arg3);
+		// throw new PendingException();
+	}
+
+}
